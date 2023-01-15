5a24e0ad021f4504ef0fd24388f9b2d2f0b54f72
Tahir zaib <tahirzaib92@gmail.com>
2023-01-16 03:09:02 +0500

    small changes

--- src/app/give-away/give-away.component.ts
+++ src/app/give-away/give-away.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
 import {
   Constants,
   Messages,
   ValidationMessages,
-} from '../shared/constants/constants';
-import { ApiService } from '../shared/services/api.service';
-import { HelpersService } from '../shared/services/helpers.service';
+} from "../shared/constants/constants";
+import { ApiService } from "../shared/services/api.service";
+import { HelpersService } from "../shared/services/helpers.service";
 declare const FloatLabel: any;
 
 @Component({
-  selector: 'app-give-away',
-  templateUrl: './give-away.component.html',
-  styleUrls: ['./give-away.component.css'],
+  selector: "app-give-away",
+  templateUrl: "./give-away.component.html",
+  styleUrls: ["./give-away.component.css"],
 })
 export class GiveAwayComponent implements OnInit {
   form: FormGroup;
@@ -45,11 +45,11 @@ export class GiveAwayComponent implements OnInit {
           Validators.maxLength(Constants.generalMaxLength),
         ],
       ],
-      instagramHandle: ['', [Validators.required]],
-      state: ['', [Validators.required]],
-      age: ['', [Validators.required, Validators.min(21)]],
-      interest: ['', [Validators.required]],
-      describe: ['', [Validators.required]],
+      instagramHandle: ["", [Validators.required]],
+      state: ["", [Validators.required]],
+      age: ["", [Validators.required, Validators.min(21)]],
+      interest: ["", [Validators.required]],
+      describe: ["", [Validators.required]],
     });
   }
 
@@ -64,8 +64,8 @@ export class GiveAwayComponent implements OnInit {
             this.isSubmitting = false;
             this.form.reset();
             this.helpersService.success(
-              'Your request has been submitted successfully.',
-              'Thank You!'
+              "Your request has been submitted successfully.",
+              "Thank You!"
             );
             this.submitted = true;
           },
